384e37933d7aff0df2629a244e848e880c208ca6
IkHwan Im <ekankr2@naver.com>
2024-10-16 16:26:26 +0900

    Update page.tsx

--- app/recruite/page.tsx
+++ app/recruite/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Page() {
   return (
-    <article className="w-full px-4 md:px-0 pt-20 pb-32 max-w-[384px] md:w-[712px] lg:w-[850px]  md:max-w-none mx-auto text-26 md:text-30">
+    <article className="w-full min-h-screen px-4 md:px-0 pt-20 pb-32 max-w-[384px] md:w-[712px] lg:w-[850px]  md:max-w-none mx-auto text-26 md:text-30">
       <h1 className="text-28 md:text-40">인재채용</h1>
       <div className="text-14 md:text-18 pt-4">
         <p>
